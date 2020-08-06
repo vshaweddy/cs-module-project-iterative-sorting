e785e0b636f4ffbf78cda99b51c56b38e5b9c54f
Vici Shaweddy <vshaweddy@gmail.com>
2020-08-05 22:26:53 -0400

    finished the binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -10,6 +10,24 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        # check if x is present at the mid
+        if arr[mid] < target:
+            low = mid + 1
+
+        # if x is greater, ignore left half and do the right
+        elif arr[mid] > target:
+            high = mid - 1
+
+        # if x is smaller, ignore the right half
+        else: 
+            return mid
 
 
     return -1  # not found
